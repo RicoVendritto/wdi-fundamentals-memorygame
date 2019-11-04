f92ad26714fb81d0203e7a11352eff9c0e2b71ae
Richard Braamburg <richard@Richards-MBP.cable.rcn.com>
2019-11-04 09:21:10 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,24 +1,22 @@
 
 let cards = ["queen", "queen", "king", "king"];
 let cardsInPlay = [];
-let cardOne = cards[0];
-let cardTwo = cards[1];
 
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-/*
-console.log("User flipped " +cardOne);
-console.log("User flipped " +cardTwo);
-*/
-
-if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You Found A Match!");
-	} else {
-		alert("Sorry Mate! Try Again");
+let checkForMatch = () => {
+	if (cardsInPlay.length === 2) {
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+		console.log("You Found A Match!");
+		} else {
+		console.log("Sorry Mate! Try Again!");
+		}
 	}
-
-	console.log("User flipped " +cardOne);
-	console.log("User flipped " +cardTwo);
 }
+
+let flipCard = (cardId) => {
+	console.log(`User flipped ${cards[cardId]}`);
+	cardsInPlay.push(cards[cardId]);
+	checkForMatch();
+}
+
+flipCard(0);
+flipCard(2);
